f0c9b773d08cceb09ae6c688c815845442481939
hermano <thearmahermano@gmail.com>
2018-11-22 15:40:24 +0100

    quote script

---
+++ js/main.js
@@ -0,0 +1,15 @@
+  //TG QUOTES - STUPID RANDOM CONSOL LOGGER
+  var quoteArray = ["PLACE QUOTES AT END IN THIS ARRAY", "'Fukk Siss!' - Nazman", "'Cool for cats!' - Everest78", "'So you are all experts then, right?' - Tiger", "'Sir, can I pack additional ammo....!' - Dayton aka IRON HEART", "'First of first...!' - Nazman", "'We are not trained for confusion!' - hermano", "'I don't like alpha' - Bornasm", "*unintelligible grumble* - Keller"];
+  var quoteArrayRandom = 1;
+  var quoteArrayEnd = quoteArray.length;
+  setInterval(logRandomQuote, 3000);
+  function logRandomQuote() {
+    var quoteArrayRandomNew = Math.floor(Math.random() * (quoteArrayEnd - 1)) + 1;
+    if (quoteArrayRandom != quoteArrayRandomNew) {
+      console.log (quoteArray[quoteArrayRandomNew]);
+      quoteArrayRandom = quoteArrayRandomNew;
+    }
+    else {
+      logRandomQuote();
+    }
+  }
