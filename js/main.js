3aead1f493ba3f8bd4cc2425ed09ea7233478c74
hermano <thearmahermano@gmail.com>
2018-11-23 15:01:51 +0100

    Added Click instead of Hover | Text Timeout | Pulse Anim

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-//Hover-over-Symbols Text
-function hoverTextShow (hoverID)
+//Hover/Click Symbols Text Reveal
+/*function hoverTextShow (hoverID)
 {
     document.getElementById(hoverID).style.display= "block";
 }
@@ -8,10 +8,32 @@ function hoverTextHide (hoverID)
 {
     document.getElementById(hoverID).style.display= "none";
 }
+*/
 
+var clickElementID = null;
+var clickElementIdBefore = null;
+var clearTextTimeoutStatus;
 
-
-  
+function clickTextChange (clickElementID) {
+    clearTextTimoutStatus = setTimeout (clearTextAfterTimeout, 16000);
+    if (clickElementID != null && clickElementIdBefore == null) {
+        document.getElementById(clickElementID).style.display= "block";
+        clickElementIdBefore = clickElementID;
+    }
+    if (clickElementID != null && clickElementIdBefore != null) {
+        document.getElementById(clickElementIdBefore).style.display= "none";
+        document.getElementById(clickElementID).style.display= "block";
+        clickElementIdBefore = clickElementID;
+    }
+    function clearTextAfterTimeout () {
+        document.getElementById(clickElementID).style.display= "none";
+        clickElementIdBefore = null;
+        clearTextTimeoutStatus = null;
+    }
+    return;
+ }
+ 
+ 
   
 //The Regiment QUOTES - STUPID RANDOM CONSOL LOGGER
 var quoteArray = ["PLACE QUOTES AT END IN THIS ARRAY", "'Fukk Siss!' - Nazman", "'Cool for cats!' - Everest78", "'So you are all experts then, right?' - Tiger", "'Sir, can I pack additional ammo....!' - Dayton aka IRON HEART", "'First of first...!' - Nazman", "'We are not trained for confusion!' - hermano", "'I don't like alpha' - Bornasm", "*unintelligible grumble* - Keller"];
